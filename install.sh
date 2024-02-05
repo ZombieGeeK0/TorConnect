645c2bc18cc27daa5ca35a38009af89334bfc7c9
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-02-05 20:16:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,17 @@
 sudo su
 clear
+echo --------------------------------------------------------------------------------------
+echo [~] Instalando... (Espero que tengas instalado metaexploit)
+echo --------------------------------------------------------------------------------------
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python3-pip
+pip3 install os
+pip3 install colarama
+pip3 install requests
+pip3 install sys
+pip3 install torpy
+pip3 install urllib.parse
+pip3 install urllib
+echo --------------------------------------------------------------------------------------------
+echo [~] Requerimientos instalados. Usa el comando python3 main.py para inciar MultiTool.
